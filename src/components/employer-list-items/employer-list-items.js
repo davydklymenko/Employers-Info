76ff2220df1ad2aaf227c9b70e1f92c3bd702e3b
Davyd Klymenko <142334488+davydklymenko@users.noreply.github.com>
2024-01-24 11:25:16 +0200

    Allow to set increase & like

--- src/components/employer-list-items/employer-list-items.js
+++ src/components/employer-list-items/employer-list-items.js
@@ -1,30 +1,38 @@
 import './employer-list-items.css';
 
-function ListItems({name, salary, increase}) {
+function ListItems(props) {
+    const {name, salary, onToogleIncrease, onDelete, onToogleLike, increase, like} = props;
 
-    let classNames = "list-group-item d-flex justify-content-between";
-    if (increase) {
-        classNames += ' increase';
-    }
+     let classNames = "list-group-item d-flex justify-content-between";
+
+     if (increase) {
+       classNames += ' increase';
+     }
+     if (like) {
+       classNames += ' like';
+     }
 
     return(
              <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
+            <span className="list-group-item-label"
+                  onClick={onToogleLike}>{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
+            <button type="button"
+                    className="btn-cookie btn-sm "
+                    onClick={onToogleIncrease}>
                     <i className="fas fa-cookie"></i>
                 </button>
 
                 <button type="button"
-                        className="btn-trash btn-sm ">
+                        className="btn-trash btn-sm "
+                        onClick={onDelete}>
                     <i className="fas fa-trash"></i>
                 </button>
                 <i className="fas fa-star"></i>
             </div>
         </li>
-    );
+        )  
 }
 
 export default ListItems;
