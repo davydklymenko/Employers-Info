b605c5169fe8e62d775768588409500445780a42
Davyd Klymenko <142334488+davydklymenko@users.noreply.github.com>
2024-01-26 12:11:24 +0200

    Allow Search and customizate search-panel block

--- src/components/app-search-panel/app-search.js
+++ src/components/app-search-panel/app-search.js
@@ -1,11 +1,33 @@
+import { Component } from 'react';
 import './app-search.css';
 
-function AppSearchPanel() {
-    return (
-        <div className="search-panel">
-            <input className="search" placeholder="Пошук роботодавців..." type="search" name="text"/>
-        </div>
-    );
+class AppSearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+    } 
+
+    onUpdateSearch = (e) => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+    }
+
+    render () {
+        return (
+            <div className='search-panel'>
+            <input 
+            className="search" 
+            placeholder="Пошук роботодавців..." 
+            type="search" 
+            name="text"
+            value={this.state.term}
+            onChange={this.onUpdateSearch}/>
+            </div>
+        );
+    }
 }
 
 export default AppSearchPanel;
