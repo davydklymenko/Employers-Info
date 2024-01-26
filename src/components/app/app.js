f1a81327ca70dd69bd054d228435d1d7615cfb83
Davyd Klymenko <142334488+davydklymenko@users.noreply.github.com>
2024-01-26 12:09:05 +0200

    Filter & Search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,11 +13,13 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                {name: "Jeff Sutherland", salary: 900, increase: false, id:1},
+                {name: "Jeff Sutherland", salary: 900, increase: true, id:1},
                 {name: "Marie Bell Santa", salary: 1400, increase: false, id:2},
                 {name: "Znowu Galya", salary: 2000, increase: false, id:3},
                 {name: "Wham", salary: 20000, increase: false, id:4},
-            ]
+            ],
+            term: '',
+            filter: 'all'
         }
 
         this.maxId = 4;
@@ -70,22 +72,55 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter((item) => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+    filterGategories = (items, filter) => {
+        switch (filter) {
+            case 'like':
+                return items.filter(item => item.like);
+            case 'moreThan1000':
+                return items.filter(item => item.salary > 1000);
+            case 'bonus':
+                return items.filter(item => item.increase);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render () {
+        const { data, term, filter } = this.state;
         const employers = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
         const liked = this.state.data.filter(item => item.like).length;
+        const visibleData = this.filterGategories(this.searchEmp(data, term), filter);
 
         return(
             <div className="app">
                 <AppInfo name={employers} increase={increased} like={liked}/>
     
                 <div className="search-panel">
-                    <AppSearchPanel/>
-                    <AppFilter/>
+                    <AppSearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
                 </div>
     
                     <EmployersListItemAdd 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteEmployers}
                     onToogleIncrease={this.onToogleIncrease}
                     onToogleLike={this.onToogleLike}/>
